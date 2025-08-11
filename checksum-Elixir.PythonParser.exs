395fb39fd390882a410b1aac2dd8ab28a489090a
gmnikhil <gm.nikhilofficial@gmail.com>
2025-08-11 10:09:00 +0530

    update checksum

--- checksum-Elixir.PythonParser.exs
+++ checksum-Elixir.PythonParser.exs
@@ -1,4 +1,4 @@
 %{
-  "libpythonparser-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:376e7507a82285ad55bd4e5868fbb5083cbab72bd9506da869026a2e3c26271e",
-  "libpythonparser-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:5aad04ab5bffcc4a9b05b45e7930bee0d94fe71ef8f1cbbd1bb1b64dd9c0c536"
+  "libpythonparser-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:08cab6eed27f564239866661272634138715ee53b65642033c159dde5964cc84",
+  "libpythonparser-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:6f75f73e4b7d044b0398c4eba30d7d62be7e44ae5fc008c49ec211022570b528"
 }
